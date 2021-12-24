798248e43c5735887529d286d072b3a88acdb941
maz808 <mazamora808@gmail.com>
2021-12-24 15:08:31 -0600

    Added tmux startup script

---
+++ tmux_startup.zsh
@@ -0,0 +1,7 @@
+clear
+if [ -z "$TMUX" ]; then
+    echo 'List of current tmux sessions:'
+    ( tmux ls && echo -n "Select or create tmux session: " ) || ( clear ; echo -n "Create tmux session: " )
+    read tmux_session
+    tmux new -As "${tmux_session:-main}"
+fi
